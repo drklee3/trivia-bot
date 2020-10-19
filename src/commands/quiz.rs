c7215283c62465b2e706e0bfb4d9b1962f2a9f68
drklee3 <derrick@dlee.dev>
2020-10-18 18:26:49 -0700

    feat: Filter out invalid clues

--- src/commands/quiz.rs
+++ src/commands/quiz.rs
@@ -1,6 +1,6 @@
 use jservice_rs::{model::Clue, JServiceRequester};
 use serenity::collector::message_collector::MessageCollectorBuilder;
-use serenity::framework::standard::{macros::command, CommandResult, Args};
+use serenity::framework::standard::{macros::command, Args, CommandResult};
 use serenity::model::prelude::*;
 use serenity::prelude::*;
 use std::time::Duration;
@@ -27,7 +27,7 @@ pub async fn quiz(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
         }
     }
 
-    let clues = match client.get_random_clues(num_questions).await {
+    let clues = match client.get_random_clues(num_questions + 5).await {
         Ok(c) => c,
         Err(e) => {
             tracing::error!("Failed to get clue: {}", e);
@@ -37,9 +37,20 @@ pub async fn quiz(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
         }
     };
 
-    tracing::info!(?clues, "Requested clues");
+    tracing::info!("Requested {} clues", clues.len());
 
-    if clues.is_empty() {
+    let clues_filtered: Vec<Clue> = clues
+        .into_iter()
+        .filter(|c| {
+            if let Some(count) = c.invalid_count {
+                count > 0
+            } else {
+                true
+            }
+        })
+        .collect();
+
+    if clues_filtered.is_empty() {
         tracing::error!(?msg, "Fetched clues are empty",);
         let _ = msg.channel_id.say(&ctx, "Failed to fetch clues :(").await?;
 
@@ -49,8 +60,8 @@ pub async fn quiz(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
     // Save state
     game_state.channel.insert(msg.channel_id.0, true);
 
-    for (i, clue) in clues.iter().enumerate() {
-        if let Err(e) = _quiz(&ctx, &msg, &clue, (i, clues.len())).await {
+    for (i, clue) in clues_filtered.iter().enumerate() {
+        if let Err(e) = _quiz(&ctx, &msg, &clue, (i, num_questions as usize)).await {
             tracing::error!(?msg, "Quiz error: {}", e);
         }
     }
@@ -60,8 +71,13 @@ pub async fn quiz(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult
     Ok(())
 }
 
-pub async fn _quiz(ctx: &Context, msg: &Message, clue: &Clue, count: (usize, usize)) -> CommandResult {
-    let _ = msg
+pub async fn _quiz(
+    ctx: &Context,
+    msg: &Message,
+    clue: &Clue,
+    count: (usize, usize),
+) -> CommandResult {
+    let sent_clue_msg = msg
         .channel_id
         .send_message(&ctx.http, |m| {
             m.embed(|e| {
@@ -97,6 +113,8 @@ pub async fn _quiz(ctx: &Context, msg: &Message, clue: &Clue, count: (usize, usi
         })
         .await?;
 
+    tracing::info!(?clue, "Sent clue");
+
     let mut collector = MessageCollectorBuilder::new(&ctx)
         .channel_id(msg.channel_id)
         .timeout(Duration::from_secs(25))
@@ -122,6 +140,7 @@ pub async fn _quiz(ctx: &Context, msg: &Message, clue: &Clue, count: (usize, usi
                 )
                 .await;
 
+            sent_clue_msg.delete(&ctx).await?;
             return Ok(());
         } else if dist > 0.8 {
             // if the answer is pretty close, react
@@ -131,6 +150,8 @@ pub async fn _quiz(ctx: &Context, msg: &Message, clue: &Clue, count: (usize, usi
             // Don't do anything when user responds wrong
         }
     }
+
+    sent_clue_msg.delete(&ctx).await?;
 
     let _ = msg
         .reply(
